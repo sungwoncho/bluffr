62612a1d9b7c2eadcf38b67b605dd5c2aee66ce8
Sung Won Cho <mikeswcho@gmail.com>
2014-12-11 17:47:33 +1100

    Configure strong params for devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,16 @@ class ApplicationController < ActionController::Base
   # Authenticate user in all controllers using Devise
   # And whitelist the controllers that do not need authentication
   before_action :authenticate_user!
+  
+  # Devise strong parameter configuration
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:username,
+          :email, :password, :password_confirmation)
+      end
+    end
 end
